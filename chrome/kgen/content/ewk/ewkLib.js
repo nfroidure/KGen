ce8c1a4a0802c330a17d63934eb29333e829469c
Nicolas Froidure <nicolas_froidure@elitwork.com>
2012-06-16 19:56:40 +0200

    Event handlers management modified

--- chrome/kgen/content/ewk/ewkLib.js
+++ chrome/kgen/content/ewk/ewkLib.js
@@ -1,13 +1,22 @@
 var ewkLib=
 	{
+	eventHandlerMethods: new Array(),
+	eventHandlers: new Array(),
 	newEventHandler: function(obj,method)
 		{
-		var fx = method;
-		return function () { return fx.apply(obj, arguments); }
+		var index=ewkLib.eventHandlerMethods.indexOf(method);
+		if(index===-1)
+			{
+			var fx=function () { return method.apply(obj, arguments); }
+			ewkLib.eventHandlerMethods.push(method);
+			ewkLib.eventHandlers.push(fx);
+			index=ewkLib.eventHandlerMethods.length-1;
+			}
+		return ewkLib.eventHandlers[index];
 		},
 	dump: function(message)
 		{
-	  Components.classes["@mozilla.org/consoleservice;1"]
-			 .getService(Components.interfaces.nsIConsoleService).logStringMessage("Ewk: " + message);
+		Components.classes["@mozilla.org/consoleservice;1"]
+			.getService(Components.interfaces.nsIConsoleService).logStringMessage("Ewk: " + message);
 		}
 	};
